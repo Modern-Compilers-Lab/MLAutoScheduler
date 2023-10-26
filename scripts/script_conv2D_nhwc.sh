782ac130b168214a6c90652644209e92b454162a
Iheb Aouadj <ia2280@dn034.fast>
2023-10-26 10:42:51 +0400

    feat: adding scripts for slurm job submition

---
+++ scripts/script_conv2D_nhwc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -p compute
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=28
+#############SBATCH --cpus-per-task=128
+#SBATCH -t 7-0:00:00
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+#SBATCH --mem=64G
+#SBATCH --reservation=c2
+
+WORKER_NUM=$((SLURM_JOB_NUM_NODES - 1))
+PORT=6379
+CONDA_DIR=/share/apps/NYUAD5/miniconda/3-4.11.0
+CONDA_ENV=/home/ia2280/.conda/envs/mlir
+RAY_OBJECT_STORE_ALLOW_SLOW_STORAGE=1
+RAY_ALLOW_SLOW_STORAGE=1
+pwd=$(pwd)
+
+. $CONDA_DIR/bin/activate
+conda activate $CONDA_ENV
+
+cmake --build .
+export LLVM_PATH=/scratch/ia2280/LLVM/llvm-project
+export SHARED_LIBS=/scratch/ia2280/LLVM/llvm-project/build/lib/libmlir_runner_utils.so,/scratch/ia2280/LLVM/llvm-project/build/lib/libmlir_c_runner_utils.so,/scratch/ia2280/LLVM/llvm-project/build/lib/libomp.so
+export AS_VERBOSE=1
+./bin/AutoSchedulerML ../benchmarks/conv2d_nhwc_hwcf.mlir
+
+
+
+
+
