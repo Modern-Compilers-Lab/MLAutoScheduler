a914a0ce37ecfbab6c2b329ddc7063c8e2de2b3a
Iheb Aouadj <ia2280@dn403.fast>
2023-11-06 17:28:50 +0400

    feat : adding beam search as a method to explore the space

---
+++ include/SearchMethod.h
@@ -0,0 +1,21 @@
+//===----------------------- SearchMethod.h -------------------------------===//
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of the SearchMethod class, which  
+/// contains an abstract definition of the methods to search for the best 
+/// schedule
+///
+//===----------------------------------------------------------------------===//
+
+#include "Node.h"
+#pragma once
+
+using namespace mlir;
+class SearchMethod {
+    private:
+        
+    public:
+        virtual Node * runSearchMethod(Node * root);
+};
