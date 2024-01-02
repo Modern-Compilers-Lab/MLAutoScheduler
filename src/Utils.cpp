d1f2206c61831a22515ff84cb4921448d11c6bda
Iheb Aouadj <ia2280@dn440.fast>
2024-01-02 15:30:20 +0400

    TEMP feat: adding other candidate generation functions

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -226,3 +226,58 @@ generateTileForAllOpCombinations(int64_t maxNumberLoops,
   return llvm::SmallVector<llvm::SmallVector<int64_t, 4>, 4>(combinations.begin(),
                                                              combinations.end());
 }
+
+
+std::vector<std::vector<unsigned>> generateCandidates(int64_t numLoops,
+                                                      int64_t NbElement)
+{
+  std::vector<std::vector<unsigned>> candidates;
+
+  if (numLoops <= 0)
+  {
+    return candidates; // Return an empty vector if numLoops is invalid
+  }
+
+  std::vector<unsigned> values(numLoops);
+  for (unsigned i = 0; i < numLoops; ++i)
+  {
+    values[i] = i;
+  }
+
+  std::vector<unsigned> currentCandidate(numLoops);
+  generateCandidateHelper(values, currentCandidate, candidates, 0);
+  std::vector<std::vector<unsigned>> out;
+  std::sample(
+      candidates.begin(),
+      candidates.end(),
+      std::back_inserter(out),
+      10,
+      std::mt19937{std::random_device{}()});
+  return out;
+  // return candidates;
+}
+
+void generateCandidateHelper(std::vector<unsigned> &values,
+                             std::vector<unsigned> &currentCandidate,
+                             std::vector<std::vector<unsigned>> &candidates,
+                             unsigned index)
+{
+  if (index == values.size())
+  {
+    // Add the completed candidate to the list
+    candidates.push_back(currentCandidate);
+    return;
+  }
+
+  for (unsigned i = 0; i < values.size(); ++i)
+  {
+    if (values[i] != UINT_MAX)
+    {
+      currentCandidate[index] = values[i];
+      unsigned temp = values[i];
+      values[i] = UINT_MAX; // Mark the value as used
+      generateCandidateHelper(values, currentCandidate, candidates, index + 1);
+      values[i] = temp; // Restore the value
+    }
+  }
+}
