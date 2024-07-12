6a867333344d2da6ac0906c3bc3c29612ac6da34
Iheb Aouadj <ia2280@login3.fast>
2024-07-12 14:29:43 +0400

    fix : adding lowering math dialect

--- src/EvaluationByExecution.cpp
+++ src/EvaluationByExecution.cpp
@@ -23,19 +23,20 @@ EvaluationByExecution::EvaluationByExecution()
 }
 EvaluationByExecution::EvaluationByExecution(std::string LogsFileName)
 {
-  this->LogsFileName = LogsFileName;
+    this->LogsFileName = LogsFileName;
 }
 std::string EvaluationByExecution::evaluateTransformation(Node *node)
 {
     std::string str1;
     llvm::raw_string_ostream output(str1);
-
+ 
     MLIRCodeIR *CodeIr = (MLIRCodeIR *)node->getTransformedCodeIr();
-    MLIRCodeIR* ClonedCode =  (MLIRCodeIR*)CodeIr->cloneIr();
-    
-    //Operation *ClonedTarget = ((Operation *)(*node->getTransformedCodeIr()).getIr());
+    MLIRCodeIR *ClonedCode = (MLIRCodeIR *)CodeIr->cloneIr();
+
+    // Operation *ClonedTarget = ((Operation *)(*node->getTransformedCodeIr()).getIr());
     mlir::Operation *op = ((mlir::Operation *)(*(ClonedCode))
-                                     .getIr());
+                               .getIr());
+
     // Printing the transformed code
     if (std::getenv("AS_VERBOSE") != nullptr)
     {
@@ -71,6 +72,7 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
             }
         }
     }
+ 
     /*mlir::PassManager pmBefore((*op).get()->getName());
 
     // Apply any generic pass manager command line options and run the pipeline.
@@ -95,10 +97,9 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
     if (!mlir::failed(pmBefore.run(*(*op))))*/
 
     // TEMP : Transforming the code using the transform dialect interpreter; it uses a system call, and applies lowerings to the results of the vectorization
-    //op->print(output);
-   
+    // op->print(output);
 
-   //std::string transformDialectString = "transform.sequence failures(propagate) { \n ^bb1(%variant_op: !transform.any_op): \n %f = transform.structured.match ops{[\"func.func\"]} in %variant_op : (!transform.any_op) -> !transform.any_op \n transform.apply_patterns to %f {  \n transform.apply_patterns.vector.lower_contraction lowering_strategy = \"outerproduct\" \n transform.apply_patterns.vector.transfer_permutation_patterns \n transform.apply_patterns.vector.lower_multi_reduction lowering_strategy = \"innerparallel\" \n transform.apply_patterns.vector.split_transfer_full_partial split_transfer_strategy = \"vector-transfer\" \n transform.apply_patterns.vector.transfer_to_scf max_transfer_rank = 1 full_unroll = true \n transform.apply_patterns.vector.lower_transfer max_transfer_rank = 1 \n transform.apply_patterns.vector.lower_shape_cast \n transform.apply_patterns.vector.lower_transpose lowering_strategy = \"shuffle_1d\" \n transform.apply_patterns.canonicalization} \n : !transform.any_op}";
+    // std::string transformDialectString = "transform.sequence failures(propagate) { \n ^bb1(%variant_op: !transform.any_op): \n %f = transform.structured.match ops{[\"func.func\"]} in %variant_op : (!transform.any_op) -> !transform.any_op \n transform.apply_patterns to %f {  \n transform.apply_patterns.vector.lower_contraction lowering_strategy = \"outerproduct\" \n transform.apply_patterns.vector.transfer_permutation_patterns \n transform.apply_patterns.vector.lower_multi_reduction lowering_strategy = \"innerparallel\" \n transform.apply_patterns.vector.split_transfer_full_partial split_transfer_strategy = \"vector-transfer\" \n transform.apply_patterns.vector.transfer_to_scf max_transfer_rank = 1 full_unroll = true \n transform.apply_patterns.vector.lower_transfer max_transfer_rank = 1 \n transform.apply_patterns.vector.lower_shape_cast \n transform.apply_patterns.vector.lower_transpose lowering_strategy = \"shuffle_1d\" \n transform.apply_patterns.canonicalization} \n : !transform.any_op}";
     /*std::string transformedString = getTransformedCode(str1, transformDialectString);
     if (transformedString == "process did not exit normally")
     {
@@ -106,19 +107,17 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
     }*/
     // TEMP : The interpreter introduces an extra "module {}", removing it
     // std::string cleanedString = removeExtraModuleTagCreated(transformedString);
-    //std::cout<<cleanedString<<std::endl;
+    // std::cout<<cleanedString<<std::endl;
     // Continue the lowerings of the code to llvm runnable code*/
-    //std::string transformDialectString = "transform.sequence failures(propagate) { \n ^bb1(%variant_op: !transform.any_op): \n %named_conv = transform.structured.match ops{[\"linalg.matmul\"]} in %variant_op : (!transform.any_op) -> !transform.any_op \n %forall_l1, %conv_l1 = transform.structured.tile_to_forall_op %named_conv tile_sizes [10, 10]: (!transform.any_op) -> (!transform.any_op, !transform.any_op) }";
+    // std::string transformDialectString = "transform.sequence failures(propagate) { \n ^bb1(%variant_op: !transform.any_op): \n %named_conv = transform.structured.match ops{[\"linalg.matmul\"]} in %variant_op : (!transform.any_op) -> !transform.any_op \n %forall_l1, %conv_l1 = transform.structured.tile_to_forall_op %named_conv tile_sizes [10, 10]: (!transform.any_op) -> (!transform.any_op, !transform.any_op) }";
 
-  
-    //mlir::OwningOpRef<Operation *> module = parseSourceString(transformDialectString, (op)->getContext());
+    // mlir::OwningOpRef<Operation *> module = parseSourceString(transformDialectString, (op)->getContext());
     //(*module)->dump();
     std::string outString;
     llvm::raw_string_ostream output_run(outString);
 
-
     std::string transformDialectString = "module attributes {transform.with_named_sequence} { \n transform.named_sequence @__transform_main(%variant_op: !transform.any_op {transform.readonly})  { %f = transform.structured.match ops{[\"func.func\"]} in %variant_op : (!transform.any_op) -> !transform.any_op \n transform.apply_patterns to %f {  \n transform.apply_patterns.vector.lower_contraction lowering_strategy = \"outerproduct\" \n transform.apply_patterns.vector.transfer_permutation_patterns \n transform.apply_patterns.vector.lower_multi_reduction lowering_strategy = \"innerparallel\" \n transform.apply_patterns.vector.split_transfer_full_partial split_transfer_strategy = \"vector-transfer\" \n transform.apply_patterns.vector.transfer_to_scf max_transfer_rank = 1 full_unroll = true \n transform.apply_patterns.vector.lower_transfer max_transfer_rank = 1 \n transform.apply_patterns.vector.lower_shape_cast \n transform.apply_patterns.vector.lower_transpose lowering_strategy = \"shuffle_1d\" \n transform.apply_patterns.canonicalization} \n : !transform.any_op \n transform.yield}}";
-    std::cout << "START VECT\n";
+    // std::cout << "START VECT\n";
 
     mlir::transform::TransformOptions options1;
     mlir::OwningOpRef<mlir::ModuleOp> moduleFromFile = parseSourceString<mlir::ModuleOp>(transformDialectString, op->getContext());
@@ -129,21 +128,20 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
         op, transformEntryPoint, *moduleFromFile,
         options1.enableExpensiveChecks(false));
 
-    //auto start = std::chrono::high_resolution_clock::now();
+    // auto start = std::chrono::high_resolution_clock::now();
     mlir::PassManager pm((op)->getName());
-    
-    
+
     // Apply any generic pass manager command line options and run the pipeline.
     applyPassManagerCLOptions(pm);
-    
-    bufferization::OneShotBufferizationOptions options;
-    //options.allowReturnAllocs = true;
-    options.bufferizeFunctionBoundaries = true;
-    //options.createDeallocs = true;
-    options.setFunctionBoundaryTypeConversion(mlir::bufferization::LayoutMapOption::IdentityLayoutMap);
-    //pm.addPass(createTransformDialectInterpreterPass(transformDialectString));
 
-    //pm.addPass(createTestTransformDialectEraseSchedulePass());
+    bufferization::OneShotBufferizationOptions options;
+    // options.allowReturnAllocs = true;
+    options.bufferizeFunctionBoundaries = true;
+    // options.createDeallocs = true;
+    options.setFunctionBoundaryTypeConversion(mlir::bufferization::LayoutMapOption::IdentityLayoutMap);
+    // pm.addPass(createTransformDialectInterpreterPass(transformDialectString));
+
+    // pm.addPass(createTestTransformDialectEraseSchedulePass());
     pm.addPass(mlir::createLoopInvariantCodeMotionPass());
     pm.addPass(mlir::createCSEPass());
     pm.addPass(mlir::createCanonicalizerPass());
@@ -151,16 +149,16 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
 
     pm.addPass(mlir::bufferization::createEmptyTensorEliminationPass());
     pm.addPass(mlir::bufferization::createEmptyTensorToAllocTensorPass());
-    
+
     pm.addPass(mlir::bufferization::createOneShotBufferizePass(options));
 
     mlir::OpPassManager &optPM = pm.nest<mlir::func::FuncOp>();
-    
+
     optPM.addPass(mlir::bufferization::createBufferDeallocationPass());
-    //optPM.addPass(mlir::bufferization::createFinalizingBufferizePass());
-    //pm.addPass(mlir::createBufferizationToMemRefPass());
-    //optPM.addPass(mlir::bufferization::createBufferDeallocationPass());
-    
+    // optPM.addPass(mlir::bufferization::createFinalizingBufferizePass());
+    // pm.addPass(mlir::createBufferizationToMemRefPass());
+    // optPM.addPass(mlir::bufferization::createBufferDeallocationPass());
+
     optPM.addPass(mlir::createConvertLinalgToLoopsPass());
     optPM.addPass(mlir::createForEachThreadLowering());
     pm.addPass(mlir::createConvertVectorToSCFPass());
@@ -171,10 +169,11 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
     pm.addPass(mlir::createFinalizeMemRefToLLVMConversionPass());
     pm.addPass(mlir::createConvertSCFToCFPass());
     pm.addPass(mlir::createLowerAffinePass());
+    pm.addPass(createConvertMathToLLVMPass());
     optPM.addPass(mlir::createArithToLLVMConversionPass());
- 
+
     pm.addPass(createConvertOpenMPToLLVMPass());
-     pm.addPass(createConvertVectorToLLVMPass());
+    pm.addPass(createConvertVectorToLLVMPass());
     pm.addPass(createConvertControlFlowToLLVMPass());
     pm.addPass(mlir::createConvertFuncToLLVMPass());
     pm.addPass(mlir::createReconcileUnrealizedCastsPass());
@@ -183,18 +182,18 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
         (op)->print(output_run);
     /*auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);*/
-    
-
 
     // Getting the evaluation uisng mlir-cpu-runner, the function uses a system call
-    //auto start_eval = std::chrono::high_resolution_clock::now();
+    // auto start_eval = std::chrono::high_resolution_clock::now();
+
     std::string OutputData = getEvaluation(outString);
-        //op->dump();
-   
+
+    // op->dump();
+
     /*auto end_eval = std::chrono::high_resolution_clock::now();
     auto duration_eval = std::chrono::duration_cast<std::chrono::microseconds>(end_eval - start_eval);*/
-    
-    // Printing the evaluation 
+
+    // Printing the evaluation
     if (std::getenv("AS_VERBOSE") != nullptr)
     {
         int asVerbose = std::stoi(std::getenv("AS_VERBOSE"));
@@ -216,8 +215,6 @@ std::string EvaluationByExecution::evaluateTransformation(Node *node)
     }
     return OutputData;
 }
-
-
 
 pid_t popen2(const char *command, int *infp, int *outfp)
 {
@@ -344,7 +341,7 @@ std::string removeExtraModuleTagCreated(std::string input) // TODO: Figure out w
 }
 
 /// Executes the evaluation command by creating a child process,
-/// feeds it the input code, captures the command's output 
+/// feeds it the input code, captures the command's output
 /// Returns the captured output as a string, optionally stripping
 /// newline characters from the output.
 
@@ -416,23 +413,27 @@ std::string getEvaluation(std::string inputCode)
     {
         int exit_status = WEXITSTATUS(status);
         printf("Cpu Runner Child process exited with status: %d\n", exit_status);
-         
+
         std::string evalString = "";
         std::string data(output_data.begin(), output_data.end());
 
         size_t lastGFLOPSPos = data.rfind("GFLOPS"); // Find the position of the last "GFLOPS"
-        if (lastGFLOPSPos != std::string::npos) {
-            std::string substring = data.substr(0, lastGFLOPSPos-1); // Extract the substring before the last "GFLOPS"
-            size_t spacePos = substring.rfind("GFLOPS"); // Find the position of the last space in the substring
+        if (lastGFLOPSPos != std::string::npos)
+        {
+            std::string substring = data.substr(0, lastGFLOPSPos - 1); // Extract the substring before the last "GFLOPS"
+            size_t spacePos = substring.rfind("GFLOPS");               // Find the position of the last space in the substring
 
-            if (spacePos != std::string::npos) {
-                evalString = substring.substr(spacePos + 6); // Extract the number string after the last space   
+            if (spacePos != std::string::npos)
+            {
+                evalString = substring.substr(spacePos + 6); // Extract the number string after the last space
             }
-        } else {
+        }
+        else
+        {
             std::cout << "No GFLOPS found in the input string." << std::endl;
             return "9000000000000000000";
         }
-        std::cout<<evalString<<std::endl;
+        std::cout << evalString << std::endl;
 
         return evalString;
     }
@@ -443,7 +444,7 @@ std::string getEvaluation(std::string inputCode)
     }
 }
 
-/// Transforms the input code using the transform dialect string. 
+/// Transforms the input code using the transform dialect string.
 /// The function uses the mlir-opt tool to run the transform dialect
 /// interprter, returns the transformed code
 
